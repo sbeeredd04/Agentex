f4eb5d24f3f69d738db4e3a5385fb89e0dffb645
sbeeredd04 <sbeeredd@asu.edu>
2025-03-10 14:38:08 -0700

    groq addition and pdf file cleanup

--- tailored-resume-extension/config.js
+++ tailored-resume-extension/config.js
@@ -2,7 +2,8 @@
 console.log('Loading config.js');
 
 const config = {
-  GEMINI_API_KEY: 'AIzaSyCsVeBSru9Wu51L9QA8EIjWlP2_Zow4FC8'
+  GEMINI_API_KEY: 'AIzaSyCsVeBSru9Wu51L9QA8EIjWlP2_Zow4FC8',
+  GROQ_API_KEY: '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',
 };
 
 // Make it available globally instead of using export
